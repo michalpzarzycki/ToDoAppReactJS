839e2d22449f0d23b31ec805b92194ce7d3f7974
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-12-13 13:53:40 +0100

    Created main components templates

---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+    render() {
+        return(
+            <div>INPUT</div>
+        );
+    }
+}
+
+export default Input;
