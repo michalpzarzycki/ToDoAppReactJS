79b301b887c71b08f66cd2683c2dbfc7b51a507f
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-14 00:21:42 +0100

    Created List.module.css

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import Item from './Item';
+import styles from './Item.module.css'
 
 class List extends Component {
     render() {
         const { items, deleteAllList, itemDelete } = this.props;
-        console.log(items);
-        return(
-            <div style={{  overflow:"scroll"}}>
-              {
-    items.map(item => {
-        return(
-            <Item key={item.id} item={item.item} id={item.id} date={item.date} value={item.value} deadline={item.deadline}  itemDelete={() => itemDelete(item.id)}/>
-            
-        )
-    })
-}
-
-<button className="btn btn-outline-danger"onClick={deleteAllList}>DELETE ALL ITEMS</button>
-           
-             
+        return (
+            <div style={{ overflow: "scroll" }}>
+                {
+                    items.map(item => {
+                        return (
+                            <Item key={item.id} item={item.item} id={item.id} date={item.date} value={item.value} deadline={item.deadline} itemDelete={() => itemDelete(item.id)} />
+                        )
+                    })
+                }
+                <button className="btn btn-outline-danger" onClick={deleteAllList}>DELETE ALL ITEMS</button>
             </div>
         );
     }
